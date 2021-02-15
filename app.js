51a76126e58e9de74e413fae93f074fba336eaa0
Imgopz <rrgkmech@gmail.com>
2021-02-15 06:49:05 +0530

    reviews add, view and delete

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
-const Review = require('./models/Review');
+const Review = require('./models/review');
 
 
 // Mongo db connection string
@@ -110,6 +110,13 @@ app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res)
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
 }));
+
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId }});
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+}))
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404));
